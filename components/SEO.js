1757a8cce48fc90d1ddae924e35429b02261e22a
Tom <web@tomcanham.co.uk>
2024-03-06 11:54:46 +0000

    Fixed a bug in SEO component that was generating an incorrect page title

--- components/SEO.js
+++ components/SEO.js
@@ -2,20 +2,9 @@ import Head from 'next/head';
 import PropTypes from 'prop-types';
 
 export default function SEO({ title }) {
-  let env = '';
-
-  if (
-    process.env.NEXT_PUBLIC_ENVIRONMENT === 'dev' ||
-    !process.env.NEXT_PUBLIC_ENVIRONMENT
-  ) {
-    env = 'DEV:';
-  } else {
-    env = '';
-  }
-
   return (
     <Head>
-      <title>{`${env}RUBL - ${title}`}</title>
+      <title>{`RUBL - ${title}`}</title>
     </Head>
   );
 }
